83dfb4b94c604d7daed79912b7bf638a87e17f17
The-Quantum-Boy <dhanorkar.sumit.code@gmail.com>
2023-05-10 22:35:57 +0530

    google auth is performed

---
+++ app/layout.jsx
@@ -0,0 +1,30 @@
+"use client";
+
+import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+export const metadata = {
+  title: "Promptopia",
+  description: "discover & Share AI Prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
